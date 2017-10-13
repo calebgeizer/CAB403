6ff2514e2c62b33441ab4315d2feaa1cc4a81e1a
calebgeizer <caleb.geizer@gmail.com>
2017-10-13 20:14:50 +1000

    Hangman Text
    Loads & tokenises “hangman_text.txt” file correctly

---
+++ Code/Hangman.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+	#define MAXDATASIZE 100
+	#define hangman_text 2
+
+
+int main() {
+	FILE *ptr_file;
+
+	char buf[1000];
+	
+	char *username[MAXDATASIZE][hangman_text];
+	
+	char *token1;
+	int i =0 ,j=0;
+	ptr_file =fopen("hangman_text.txt","r");
+	
+	if (!ptr_file)
+		return 1;
+
+	while (fgets(buf,1000, ptr_file)!=NULL) {
+		
+		token1=strtok(buf,"		");		
+		username[j][i]=token1;		
+		printf("%s\n",username[j][i]);
+		i++;
+	}
+	
+		
+	fclose(ptr_file);
+	
+	
+	return 0;
+}
