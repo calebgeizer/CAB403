e9f3340d7d614e8b978235076fe0f9badf08377b
calebgeizer <caleb.geizer@gmail.com>
2017-10-13 20:59:26 +1000

    Hangman - Fixed segmentation fault

--- Code/Hangman.c
+++ Code/Hangman.c
@@ -1,9 +1,33 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-	#define MAXDATASIZE 100
+	#define MAXDATASIZE 200
 	#define hangman_text 2
+/*
+char twoWords(char *hangmanWords[MAXDATASIZE][hangman_text]){
+	int size = sizeof(hangmanWords);
 
+	//select random word
+	int x = rand()%size;
+	int y = rand()%size;
+
+	//check if it's the same word
+	while(x == y){
+		int y = rand()%size;
+	}
+
+
+	char *word1 = hangmanWords[x][0];
+	char *word2 = hangmanWords[y][0];
+
+	char* finalText;
+	finalText = malloc(strlen(word1)+1+4); *//* make space for the new string (should check the return value ...) */
+	//strcpy(finalText, word1); /* copy name into the new var */
+	//strcat(finalText, word2); /* add the extension */
+
+	//printf("%s\n", finalText);
+	//return *finalText;
+//}
 
 int main() {
 	FILE *ptr_file;
