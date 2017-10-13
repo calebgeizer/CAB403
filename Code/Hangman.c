8d39bb9e06f68f4eaa747315b013d06e8ed0e15d
calebgeizer <caleb.geizer@gmail.com>
2017-10-13 21:22:15 +1000

    Hangman

--- Code/Hangman.c
+++ Code/Hangman.c
@@ -1,30 +1,27 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <string.h>
-	#define MAXDATASIZE 250
-	#define hangman_text 2
+	#define MAXDATASIZE 500
+	#define hangman_text 10
 
 char twoWords(char *hangmanWords[MAXDATASIZE][hangman_text],int size){
 
 	//select random word
-	int x = rand()%size;
-	int y = rand()%size;
+	int x = rand() % size;
+	int y = rand() % size;
 
-	//check if it's the same word
-	while(x == y){
-		int y = rand()%size;
-	}
-
+	//printf("%d %d\n", x,y);
 
 	char *word1 = hangmanWords[x][0];
 	char *word2 = hangmanWords[y][0];
+	//printf("%s %s\n", word1, word2);
 
 	char* finalText;
 	finalText = malloc(strlen(word1)+1+4); /* make space for the new string (should check the return value ...) */
 	strcpy(finalText, word1); /* copy name into the new var */
 	strcat(finalText, word2); /* add the extension */
 
-	printf("%s\n", finalText);
+	//printf("%s\n", finalText);
 	return *finalText;
 }
 
@@ -57,6 +54,17 @@ int main() {
 		i--;		
 		size++;
 	}
+
+
+	
+	for (int i = 0; i < size; ++i)
+	{
+		printf("%s\n", hangmanWords[0][i]);
+	}
+
+	
+
+	//printf("%d\n", size);
 
 	char finalText = twoWords(hangmanWords,size);
 	
